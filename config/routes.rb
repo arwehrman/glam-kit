8cd90473b1ca1b9380bff01e46c1976b2cda5910
arwehrman <arwehrman@gmail.com>
2017-12-05 22:22:31 -0500

    changed routes back to original

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
+  get '/' => 'sessions#welcome', as: 'welcome'
+  get '/signin' => 'sessions#new', as: 'signin'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
+  
   resources :items
   resources :kits
   resources :users
-
-  end
-
 
 end
