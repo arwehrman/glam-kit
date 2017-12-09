be272f4798d6f21e133154222eac42d4f5af0506
arwehrman <arwehrman@gmail.com>
2017-12-09 17:40:54 -0500

    added Item controller and views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,36 @@
 class ItemsController < ApplicationController
-  
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+  def item_params
+  end
+
 
 
 end
