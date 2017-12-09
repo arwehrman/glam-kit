ea5d901acacfcfdd318edd3d67add53fd5166995
arwehrman <arwehrman@gmail.com>
2017-12-09 16:15:43 -0500

    got signin to redirect properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:user][:username])
-    if @user.authenticate(params[:user][:password])
+    if @user = User.find_by(username: params[:user][:username])
+    @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       redirect_to '/signin'
+      #render message on redirec to let person know
     end
   end
 
