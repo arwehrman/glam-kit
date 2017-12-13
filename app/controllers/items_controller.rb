8b6f328bc4620c040c2e86f3b3717949547ebec2
arwehrman <arwehrman@gmail.com>
2017-12-13 11:09:49 -0500

    added form partial eventually will use

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,24 @@
 class ItemsController < ApplicationController
 #need the kit_id to associate items with a kit
   def new
-    # if params[:kit_id]
-    @kit = Kit.find(:kit_id params[:id])
+    @kit = Kit.find(params[:kit_id])
     @item = Item.new
-
   end
 
   def edit
+    @kit = Kit.find(params[:kit_id])
     @item = Item.find(params[:id])
   #need to get the kit-id to pull
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
   def update
     @item = Item.find(params[:id])
-    @item.update_attributes
+    @item.update(item_params)
 
   end
 
@@ -22,4 +26,8 @@ class ItemsController < ApplicationController
     Item.find(params[:id]).destroy
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:name, :brand, :color, :price, :color, :rating, :comment, :kit_id, :category_id)
+  end
 end
