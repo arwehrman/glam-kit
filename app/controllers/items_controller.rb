74ad99fdf76aa1d4222126b99c41659d7331d840
arwehrman <arwehrman@gmail.com>
2017-12-12 22:01:34 -0500

    stuck on Patch error for edit item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,12 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    @kit = Kit.find(params[:id])
   end
 
   def update
     @item = Item.find(params[:id])
+
   end
 
 
