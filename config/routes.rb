7c6428005147549bcc5414701c514201268ba20a
arwehrman <arwehrman@gmail.com>
2017-12-13 08:42:09 -0500

    adding notes to forms

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :show, :create]
     resources :kits do
-      resources :items
+      resources :items, only: [:edit, :update, :destroy] 
     end
 end
