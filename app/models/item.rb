04748e18bca4d1b9df3ca305186e03bb2f80aba6
arwehrman <arwehrman@gmail.com>
2018-01-09 20:45:28 -0500

    removed unnecessary association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :kit
   belongs_to :category
-  has_many :users, through: :kits
+
 
   validates :rating, inclusion: { in: 1..5 }
   validates :name, presence: true
