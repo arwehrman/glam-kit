b7d0669abe30da90b9118146ed17e0e8544a716f
arwehrman <arwehrman@gmail.com>
2018-01-14 15:21:27 -0500

    removed Ranking validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   belongs_to :kit
   belongs_to :category
   
-  validates :rating, inclusion: { in: 1..5 }
   validates :name, presence: true
 
   def self.by_category(category_id)
