8872724018c60ef4fe48f9013ac6bb72cef96087
arwehrman <arwehrman@gmail.com>
2018-03-11 21:56:30 -0400

    added see more items button but not actually working

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :brand
+  attributes :id, :name, :brand, :category_id, :color, :price, :rating, :comment
   belongs_to :kit
   belongs_to :category
 end
--- app/serializers/kit_serializer.rb
+++ app/serializers/kit_serializer.rb
@@ -1,5 +1,6 @@
 class KitSerializer < ActiveModel::Serializer
   attributes :id, :name
+  
   has_many :items
   has_many :categories, through: :items
 end
