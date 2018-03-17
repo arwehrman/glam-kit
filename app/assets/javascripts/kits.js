c93122eb980c2699ed84697fd8ab78e2fdb3ec4d
arwehrman <arwehrman@gmail.com>
2018-03-16 21:59:59 -0400

    remvoved old unused code and updated form. form still not working

--- app/assets/javascripts/kits.js
+++ app/assets/javascripts/kits.js
@@ -3,8 +3,8 @@ $(function() {
     var id = $(this).data("id");
     $.get("/kits/" + id + ".json", function(data) {
       var kit = data;
-      var kitText = "<p>" + kit["name"] + "</p>";
-      $("#kit-" + id).html(kitText);
+      /*var kitText = "<p>" + kit["name"] + "</p>";
+        $("#kit-" + id).html(kitText); */
       var items = kit["items"];
       var itemList = "";
       items.forEach(function(item) {
@@ -14,7 +14,3 @@ $(function() {
     });
   });
 });
-
-//Things to do
-
-//hide items when button is clicked again?
