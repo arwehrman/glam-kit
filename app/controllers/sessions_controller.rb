fbf8fec7c6f3decb20be036085b0402101dbea91
arwehrman <arwehrman@gmail.com>
2018-12-16 12:37:53 -0500

    decided to remove Facebook signon

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,6 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    if auth_hash = request.env["omniauth.auth"]
-      @user = User.find_or_create_by_omniauth(auth_hash)
-      session[:user_id] = @user.id
-      redirect_to kits_path(@user)
-    end
-  end
-
   def login
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,4 @@ class User < ApplicationRecord
   validates_format_of :email, :with => /\A[^@,\s]+@[^@,\s]+\.[^@,\s]+\z/
   validates :password, presence: true, length: {minimum: 6}
 
-  def self.find_or_create_by_omniauth(auth_hash)
-    oauth_email = auth_hash["info"]["email"]
-    where(:email => oauth_email).first_or_create do |user|
-        user.password = SecureRandom.hex
-        user.name = auth_hash.info.name
-    end
-  end
 end
